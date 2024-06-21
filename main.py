302bf7d6da4f9e4d010a37183ab8bcf8e0c09d1e
Kemal Özyön <kemalozyon51@gmail.com>
2024-06-21 15:14:22 +0300

    main.py updated

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from PIL import Image
 
 inputPath = input("Dosyanın Yolunu Giriniz: ")
 outputPath = input("Varsayılan yere mi kaydedilsin Y/N?")
+while True:
+    if outputPath == "y" or outputPath == "Y":
+        fileName = input("Çıktı dosyasının adını giriniz: ")
+        outputPath = f"/Users/kemalozyon/Downloads/BackgroundImageDeleted/{fileName}.png"
+        break
 
-if outputPath == "y" or outputPath == "Y":
-    fileName = input("Çıktı dosyasının adını giriniz: ")
-    outputPath = f"/Users/kemalozyon/Downloads/BackgroundImageDeleted/{fileName}.png"
+    elif outputPath == "n" or outputPath == "N":
+        myPath = input("Kaydedilecek yerin adresini giriniz")
+        fileName = input("Çıktı dosyasının adını giriniz: ")
+        outputPath = input(myPath+f"{fileName}.png")
+        break
+    else:
+        print("Söylenen sınırlar içinde cevap verin!!!")
+        outputPath = input("Varsayılan yere mi kaydedilsin Y/N?")
 
-elif outputPath == "n" or outputPath == "N":
-    myPath = input("Kaydedilecek yerin adresini giriniz")
-    fileName = input("Çıktı dosyasının adını giriniz: ")
-    outputPath = input(myPath+f"{fileName}.png")
 
 input1 = Image.open(inputPath)
 output1 = remove(input1)
